97c20e1e2afe1a3a0c15ba7243a40cd61ad085ef
Oliver <oliveroneill04@gmail.com>
2018-02-03 18:06:45 +1100

    Initial Commit

---
+++ Ash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ash
+//
+//  Created by Oliver ONeill on 3/2/18.
+//  Copyright © 2018 Oliver ONeill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
